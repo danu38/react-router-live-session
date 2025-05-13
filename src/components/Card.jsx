3847185b4f9f259d32961786273c6a3951e3ce4d
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-05-13 16:45:23 +0200

    files initiated

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import './Card.css'
-
-const Card = ({ movie }) => {
-  return (
-    <div className="card">{movie}</div>
-  )
-}
-
-export default Card
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import './Header.css'
-
-const Header = () => {
-  return (
-    <header>Header</header>
-  )
-}
-
-export default Header
