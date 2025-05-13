c551ea578170b8d7979f394080ac35cf0e8b6d19
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-05-13 18:00:56 +0200

    MovieCard structure added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const MovieCard = () => {
-  return <div></div>;
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie-card">
+      <img
+        src={`https://image.tmdb.org/t/p/w342${movie.backdrop_path}`}
+        alt={movie.title}
+        loading="lazy"
+      />
+      <h2>{movie.title}</h2>
+      <p>⭐ {movie.vote_average}</p>
+    </div>
+  );
 };
 
 export default MovieCard;
