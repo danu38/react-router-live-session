17c58e77467b366d6503b60d0cce985999d60ab9
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-05-13 17:55:37 +0200

    added html elements sturucture for home jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,40 @@
 // home.jsx is file to fetch up the all movies from the api
 // and show them in the home page
 
-
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import MovieCard from '../components/MovieCard'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
-const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
 const Home = () => {
-const[movies, setMovies] = useState([])
-const [loading, setLoading] = useState(true)
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-useEffect(() => {
+  useEffect(() => {
     fetch(API_URL)
-    .then((res) => res.json())
-    .then((data) => {
-        setMovies(data.results)
-        setLoading(false)
-    })  
-    .catch((err) => console.log(err))
-}
-, [])
-
-
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+        setLoading(false);
+        console.log(data.results);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <div>
-      Movies
-    </div>
-  )
-}
+    <main>
+      <h1>Popular Movies</h1>
+      <div>
+        {movies.map((movie) => (
+          <Link to={`/movies/${movie.id}`} key={movie.id}>
+            <MovieCard movie={movie} />
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
