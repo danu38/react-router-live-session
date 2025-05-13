6c6bea0ef866be8b091eb832f4a255f6684c0180
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-05-13 17:43:21 +0200

    fetch movies from moveis api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,19 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
 
 const Home = () => {
+const[movies, setMovies] = useState([])
+const [loading, setLoading] = useState(true)
 
-
+useEffect(() => {
+    fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+        setMovies(data.results)
+        setLoading(false)
+    })  
+    .catch((err) => console.log(err))
+}
+, [])
 
 
 
