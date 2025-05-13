33afa2681aceb5a1daea2b9c7e20be71ad649116
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-05-13 17:40:50 +0200

    define the imports and variables for home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+// home.jsx is file to fetch up the all movies from the api
+// and show them in the home page
+
+
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import MovieCard from '../components/MovieCard'
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
 
 const Home = () => {
 
 
 
 
-    
+
   return (
     <div>
       Movies
