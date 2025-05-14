00dffed290aae8ec91b3d2925b6640fb000df846
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-05-14 10:14:48 +0200

    back button added

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import Loader from '../components/Loader';
-import ErrorMessage from '../components/ErrorMessage';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader";
+import ErrorMessage from "../components/ErrorMessage";
+import { useNavigate } from "react-router-dom";
 
 // import ErrorMessage from '../components/ErrorMessage';
 
@@ -13,6 +14,7 @@ const MovieDetail = () => {
   const [movie, setMovie] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(
@@ -20,7 +22,7 @@ const MovieDetail = () => {
     )
       .then((res) => {
         if (!res.ok) {
-          throw new Error('Failed to fetch movie details');
+          throw new Error("Failed to fetch movie details");
         }
         return res.json();
       })
@@ -41,16 +43,19 @@ const MovieDetail = () => {
   if (error) {
     return <ErrorMessage />; //ErrorMessage component
   }
-
+const handleBack = () => {
+    navigate('/');
+  };
   // Om allt är OK, rendera startsidan
 
   return (
-    <div className='movie-detail'>
+    <div className="movie-detail">
+      <button onClick={handleBack}>← Back to list</button>
       <h1>{movie.title}</h1>
       <img
         src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
         alt={movie.title}
-        loading='lazy'
+        loading="lazy"
       />
       <p>{movie.overview}</p>
       <p>⭐ {movie.vote_average}</p>
